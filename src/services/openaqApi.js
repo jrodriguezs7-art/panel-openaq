da2bdfe49e3573c436fbc825eb88fb9fba6288dd
jrodriguezs7-art <jrodriguezs7@uteq.edu.ec>
2026-08-06 01:34:04 -0500

    SECOND COMMIT

--- src/services/openaqApi.js
+++ src/services/openaqApi.js
@@ -2,7 +2,7 @@ const API_URL = "/api/openaq/v3";
 
 export async function obtenerEstaciones() {
   const bbox =
-    "-83.80713775878478,-5.145582243618086,-74.34376817756562,1.776830555686696";
+    "-83.807137,-5.145582,-74.343768,1.776830";
 
   const respuesta = await fetch(
     `${API_URL}/locations?bbox=${bbox}&limit=100&page=1`
